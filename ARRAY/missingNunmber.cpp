8b3a5d3d5a529c56f1017bc0000e483c64b35848
shubham <shubhammundkar128@gmail.com>
2026-07-27 19:32:00 +0530

    missing number

---
+++ ARRAY/missingNunmber.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void missingNumber(int arr[],int n,int k){
+    bool anyMissing = false;
+    for(int i=1;i<=k;i++){
+        bool found = false;
+        for(int j=0;j<n;j++){
+            if(i==arr[j]){
+                found = true ;
+                break;
+            }
+        }
+        if(!found){
+            cout<<"Missing number is : "<<i<<endl;
+            anyMissing=true;
+        }
+        
+    }
+    if(!anyMissing)
+        cout<<"There is not any missing number ";
+}
+int main(){
+    int n,k;
+    cout<<"entter no of elements : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements in arr : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"enter number : ";
+    cin>>k;
+
+    missingNumber(arr,n,k);
+    return 0;
+}
