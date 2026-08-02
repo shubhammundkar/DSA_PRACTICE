857e3edb35b350ff63543e175c2693425537ea4f
shubham <shubhammundkar128@gmail.com>
2026-08-02 17:25:00 +0530

    basics of ll like singly ll listnode implementation done

---
+++ LL/SINGLYLL/listnodeImplementation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+
+    ListNode(int x){
+        val = x;
+        next=nullptr;
+    }
+};
+int main(){
+    ListNode* first = new ListNode(5);
+    ListNode* second = new ListNode(10);
+    ListNode* third = new ListNode(15);
+    ListNode* fourth = new ListNode(20);
+
+    first->next=second;
+    second->next=third;
+    third->next=fourth;
+
+    ListNode* temp=first;
+
+    while(temp!=nullptr){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+
+    return 0;
+}
